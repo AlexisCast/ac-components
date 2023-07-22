f96a6dc9a904766d8b4a14356012b74da66711f4
AlexisCast <castellanos.alexis@hotmail.com>
2023-07-21 22:37:19 -0700

    update eslint config

---
+++ src/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react';
+
+interface ButtonProps {
+  children: ReactNode;
+  variant: 'primary' | 'secondary';
+}
+
+export const Button = ({ children, ...props }: ButtonProps) => {
+  console.log('This shows errors');
+  return <button {...props}>{children}</button>;
+};
