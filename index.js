44c4bb8db956935ed023a115967cab745269eca2
AlexisCast <AlexisCast@users.noreply.github.com>
2023-07-26 21:58:14 +0000

    deploy: 906aba852aa9988370cda8a8eccb8078259563d2

--- index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./ac-components.cjs.production.min.js')
-} else {
-  module.exports = require('./ac-components.cjs.development.js')
-}
