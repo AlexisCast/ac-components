c74dd2a2a18f812a3b46f2f9e794de79af171b03
AlexisCast <castellanos.alexis@hotmail.com>
2023-08-01 16:02:09 -0700

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 import babel from '@rollup/plugin-babel';
-import tailwindcss from 'tailwindcss';
-const tailwindConfig = require('./tailwind.config.js');
-
 export default {
   input: './src/index.tsx',
   output: {
@@ -26,7 +23,6 @@ export default {
       inject: {
         insertAt: 'top',
       },
-      plugins: [tailwindcss(tailwindConfig)],
     }),
     babel({
       babelHelpers: 'bundled',
